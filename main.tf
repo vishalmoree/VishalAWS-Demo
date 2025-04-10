d90d8da24f95d29531bef97266e300df8e1d81c6
Vishal More <vishalmoree@gmail.com>
2025-04-10 10:42:11 +0530

    added secrete key

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
+terraform{
+  requires_providers{
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
 provider "aws" {
   region  = "eu-north-1"
+  access_key = "AWS_ACCESS_KEY_ID_DEMO1"      
+  secret_key = "AWS_SECRET_ACCESS_KEY_DEMO1"
   }
 
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
+  ami           = "ami-0d188df7cedce7d90"
   instance_type = "t3.micro"
 
   tags = {
