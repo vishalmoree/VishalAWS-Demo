c1f74ab56ab8b591a1346b6a5839a4896949c883
Vishal More <vishalmoree@gmail.com>
2025-04-12 12:23:12 +0530

    attaching policy and new user

--- createIAMuser.tf
+++ createIAMuser.tf
@@ -7,14 +7,42 @@ resource "aws_iam_user" "lb" {
   }
 }
 
+resource "aws_iam_user" "lb1" {
+  name = "loadbalancer1"
+  path = "/system/"
+ 
+  tags = {
+    tag-key = "load-balancer1"
+  }
+}
+
 resource "aws_iam_policy" "policy" {
   #name        = "Demo-policy"
   #description = "A Demo policy"
-  policy      = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  policy      = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*",
+                "s3-object-lambda:*"
+            ],
+            "Resource": "*"
+        }
+    ]
 }
+}
+
 
 resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
   # group      = aws_iam_group.example_group.name
 user = aws_iam_user.lb.name
-policy_arn = aws_iam_policy.policy.arn
+policy_arn = aws_iam_policy.policy.policy
+}
+
+resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
+  # group      = aws_iam_group.example_group.name
+user = aws_iam_user.lb1.name
+policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
