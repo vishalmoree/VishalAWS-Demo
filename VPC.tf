c407862d41fe7b9582ffb555d88decd626b695d3
Vishal More <vishalmoree@gmail.com>
2025-04-16 14:23:02 +0530

    adding subnet to VPC

--- VPC.tf
+++ VPC.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "main" {
     Name = "testVPC"
   }
 }
+
+#creating subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "TestSubnet1"
+  }
+}
