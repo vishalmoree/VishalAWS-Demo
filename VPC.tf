60cd91305da989478ef2519249b69fc013a54884
Vishal More <vishalmoree@gmail.com>
2025-04-25 11:48:57 +0530

    test

--- VPC.tf
+++ VPC.tf
@@ -81,10 +81,11 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
+  #from_port         = 443
+  ip_protocol       = "-1"
+  #to_port           = 443
 }
 
 resource "aws_security_group" "allow_all" {
