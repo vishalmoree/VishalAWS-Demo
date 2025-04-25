046e8baccc7f1b5505a29a56f85c5b86ed874e47
Vishal More <vishalmoree@gmail.com>
2025-04-25 11:36:48 +0530

    adding ingress rule

--- VPC.tf
+++ VPC.tf
@@ -79,6 +79,13 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
@@ -89,4 +96,5 @@ resource "aws_security_group" "allow_all" {
     Name = "allow_all"
   }
 }
+
 
