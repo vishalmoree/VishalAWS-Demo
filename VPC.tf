7105ccc64e6ddda71eedbeb01ba47b91f127d79e
Vishal More <vishalmoree@gmail.com>
2025-05-08 11:46:58 +0530

    adding route table

--- VPC.tf
+++ VPC.tf
@@ -223,7 +223,7 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.public_route.id
 }
  
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.TestSubnetPublic.id
   route_table_id = aws_route_table.public_route.id
 }
